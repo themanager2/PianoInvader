b44875d8a09edcf294b3c8f1f6f7eaf13b0faeb7
Hisham-007-debug <hishamadam101@gmail.com>
2026-04-29 18:01:33 +0100

    add class java files

---
+++ PianoInvader2/src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import javax.sound.midi.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+public class Main{
+    public static void main(String[] args) throws Exception{}
+}
